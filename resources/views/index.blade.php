fe028d5b78b927bfcbc2cb29b286b5bea7cea9c1
ImJosee <josembelamendia@gmail.com>
2019-11-08 20:40:28 -0300

    Index posts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,43 @@
 @section('title', 'Bienvenido a digitalme')
 
 @section('content')
-    @foreach($posts as $post)
-        <li>
-        {{$post->title}}
+<section>
+    <div class="descripcion">
+    <h2 class="date">Date a conocer.</h2>
+    </div>
+    </section>
+    <section>
+    <nav class="section-na navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="abajo linksection" href="index.php">Lo mejor de digitalMe</a>
         </li>
-    @endforeach
+        <li class="nav-item">
+            <a class="abajo linksection" href="index.php">Categorias<a>
+        </li>
+        <li class="nav-item">
+            <a class="abajo linksection" href="login.php">Escuelas y organizaciones</a>
+        </li>
+        </ul>
+    </nav>
+    </section>
+<main>
+    <div class="publicaciones container">
+      <div class="row">
+      @foreach($posts as $post)
+        <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="publicacion">           
+                <a href="/posts/"><img src="images/publicacion2.jpg" alt=""></a>
+                <div>
+                    <span>
+                    {{ $post->title }}.  Views: {{$post->views}}
+                    </span>
+                </div>
+            </div>
+        </div>
+      @endforeach
+      </div>
+    </div>
+
+</main>
 @endsection    
