f92a808d79bdfdcddd12a6b34f3ff56e163a4e91
Jose <josembelamendia@gmail.com>
2019-12-10 20:11:55 -0300

    Search title changed (FAQ)

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -15,11 +15,19 @@
         <section class="categories blocks">
             <div>
                 <h3 id="faq-title">
+                @if(request()->has('search'))
+                    @if(count($questions) == 0)
+                        <h2 id="search-results">No hay preguntas frecuentes relacionadas con {{request('search')}}</h2>
+                    @else
+                        <h2 id="search-results">Resultados para: {{request('search')}} (Total: {{$questions->total()}})</h2>
+                    @endif
+                @else 
                     Algunas preguntas frecuentes:
+                @endif
                 </h3>
             </div>
             <ul class="blocks-list">
-            @foreach($questions as $question)
+        @foreach($questions as $question)
                 <li class="blocks-item">
                 <a href='#' class="blocks-item-link question">
                     {{$question->question}}
